ec424096786a62f94ac36355b42c279e6b59285b
Valentin <valentin.sauer@commercetools.de>
2017-02-27 20:51:13 +0100

    Add sparql-client-2 as dependency

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-var rdf = require('rdf-ext');
+var SparqlClient = require('sparql-client-2');
 
 var Config = require('../config');
 var config = new Config();
+
+//var endpoint = 'https://query.wikidata.org/sparql?query=SPARQL';
+
+// Get the leaderName(s) of the 10 cities
+var query = "SELECT * FROM <http://dbpedia.org> WHERE { " +
+  "?city <http://dbpedia.org/property/leaderName> ?leaderName " +
+  "} LIMIT 10";
+var client = new SparqlClient( 'http://dbpedia.org/sparql')
+  .register({db: 'http://dbpedia.org/resource/'});
+
+client.query(query)
+  .bind('city', {db: 'Vienna'})
+  .execute(function(error, results) {
+    console.dir(arguments, {depth: null});
+  });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
