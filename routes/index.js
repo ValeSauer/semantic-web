6b32960276788d6e44c01c7470e09683185b803d
PeterHollrieder <Peter.Hollrieder@gmail.com>
2017-03-05 21:28:14 +0100

    Added new randomization, added three variables for the nonWinner

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ var GeoPoint = function(point){
     }
 }
 
+//here comes the query
 var query = "SELECT ?subj ?label ?coord ?elev ?picture WHERE { " +
   "?subj wdt:P2044 ?elev. " +
   "?subj wdt:P625 ?coord. " +
@@ -29,17 +30,42 @@ var query = "SELECT ?subj ?label ?coord ?elev ?picture WHERE { " +
   "FILTER(?elev > 3000)  " +
   "}" +
   "ORDER BY RAND() " +
-  "LIMIT 10";
+  "LIMIT 100";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res,/* mM1, mM2, mM3,*/ next) {
 
   var mountains = request('https://query.wikidata.org/sparql?format=json&query=' + urlencode(query), function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var result = JSON.parse(body).results.bindings[1];
+
+        //create four random numbers for our four mountains
+        var min = 1;
+        var max = 100; //this has to be set to our query limit
+
+        function rand (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        //create our four variables
+        var g = rand(min, max); // this is the chosen one
+        var o = rand(min, max); // John Doe #1
+        var i = rand(min, max); // John Doe #2
+        var l = rand(min, max); // John Doe #3
+
+      var result = JSON.parse(body).results.bindings[g]; // Changed the [1] to rand number g, if something crashes you know what to do ;-)
+      var miscMountain1 = JSON.parse(body).results.bindings[o];
+      var miscMountain2 = JSON.parse(body).results.bindings[i];
+      var miscMountain3 = JSON.parse(body).results.bindings[l];
       console.log("Request done");
       var point = new GeoPoint(result.coord.value);
+      /*var pointMM1 = new GeoPoint(miscMountain1.coord.value);
+      var pointMM2 = new GeoPoint(miscMountain2.coord.value);
+      var pointMM3 = new GeoPoint(miscMountain3.coord.value);*/
       res.render('index', { name: result.label.value, coordinates: result.coord.value ,image: result.picture.value, lat: point.lat, long: point.long });
+      /*mM1.render('index', { name: miscMountain1.label.value, coordinates: miscMountain1.coord.value ,image: miscMountain1.picture.value, lat: pointMM1.lat, long: pointMM1.long });
+      mM2.render('index', { name: miscMountain2.label.value, coordinates: miscMountain2.coord.value ,image: miscMountain2.picture.value, lat: pointMM2.lat, long: pointMM2.long });
+      mM3.render('index', { name: miscMountain3.label.value, coordinates: miscMountain3.coord.value ,image: miscMountain3.picture.value, lat: pointMM3.lat, long: pointMM3.long });*/
+      console.log(miscMountain1, miscMountain2, miscMountain3);
     }
     return false;
   })
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, /*mM1, mM2, mM3,*/ next) {
   res.send('respond with a resource');
+  mM1.send('respond with a resource');
+  mM2.send('respond with a resource');
+  mM3.send('respond with a resource');
 });
 
 module.exports = router;
