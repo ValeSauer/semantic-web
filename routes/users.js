abea996c9bcc85b82d905f716358aa790b5ade75
PeterHollrieder <Peter.Hollrieder@gmail.com>
2017-03-05 22:35:59 +0100

    delete each function in index.hbs

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, /*mM1, mM2, mM3,*/ next) {
   res.send('respond with a resource');
-  mM1.send('respond with a resource');
+  /*mM1.send('respond with a resource');
   mM2.send('respond with a resource');
-  mM3.send('respond with a resource');
+  mM3.send('respond with a resource');*/
 });
 
 module.exports = router;
